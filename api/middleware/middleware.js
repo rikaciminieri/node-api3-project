a64310e5f6e30ac12625950ab3a752beb45d60b7
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-07 19:40:10 -0700

    logger middleware added and functioning

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,7 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next()
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +17,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = logger, validateUserId, validateUser, validatePost
