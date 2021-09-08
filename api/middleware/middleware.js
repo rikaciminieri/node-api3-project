0fe34ba75070fb4e48aa4e6cf879987f42480f9b
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-07 20:17:58 -0700

    validatePost middleware written but not implemented

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -7,23 +7,28 @@ function logger(req, res, next) {
 }
 
 function validateUserId(req, res, next) {
-  Users.getById(req.params.id)
-    .then(user => {
-      if(user) {
-        next()
-      } else {
-        next({message: `User with id ${req.params.id} not found!`})
-      }
-    })
+  Users.getById(req.params.id).then((user) => {
+    if (user) {
+      next();
+    } else {
+      next({ message: `User with id ${req.params.id} not found!` });
+    }
+  });
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.name) {
+    next({ status: 400, message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text) {
+    next({ status: 400, message: "missing required text field" });
+  }
 }
 
 // do not forget to expose these functions to other modules
-module.exports = logger, validateUserId, validateUser, validatePost 
+(module.exports = logger), validateUserId, validateUser, validatePost;
