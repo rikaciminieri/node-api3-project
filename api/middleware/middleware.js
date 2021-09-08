09bd215509764a851bfd36010145e59e3cf6aef9
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-07 19:48:54 -0700

    validateUserId middleware written but not implemented

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,11 +1,20 @@
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
 
-  next()
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  Users.getById(req.params.id)
+    .then(user => {
+      if(user) {
+        next()
+      } else {
+        next({message: `User with id ${req.params.id} not found!`})
+      }
+    })
 }
 
 function validateUser(req, res, next) {
@@ -17,4 +26,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
-module.exports = logger, validateUserId, validateUser, validatePost
+module.exports = logger, validateUserId, validateUser, validatePost 
